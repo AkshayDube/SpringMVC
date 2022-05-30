794a720891f61c346f75b517c09ce78327936939
AkshayDube <dubeyakshay09@gmail.com>
2022-05-30 17:29:14 +0530

    Using @ModelAttribute for binding front_data to model_data and also Using @ModelAttribute for a common method that runs before every handler method in the same file

--- src/main/java/springMVC/controller/Signup.java
+++ src/main/java/springMVC/controller/Signup.java
@@ -2,6 +2,7 @@ package springMVC.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,23 +17,33 @@ public class Signup {
 	}
 	
 	@RequestMapping(path="/success",method = RequestMethod.POST)
-	public String success(@RequestParam("email") String email,
-			@RequestParam("username")String username,
-			@RequestParam("password")String password,
-			Model model)
-	{
-		User user=new User();
-		user.setEmail(email);
-		user.setPassword(password);
-		user.setUsername(username);
-		
-		model.addAttribute("user", user);
-		
-//		model.addAttribute("email",email);
-//		model.addAttribute("username",username);
-//		model.addAttribute("password",password);
-//		System.out.println(model);
+	public String success(@ModelAttribute User user) {
 		return "success";
 	}
-
+	
+	@ModelAttribute
+	public void check() {
+		System.out.println("Check Works Fine");
+	}
+	
+	
 }
+//@RequestMapping(path="/success",method = RequestMethod.POST)
+//public String success(@RequestParam("email") String email,
+//		@RequestParam("username")String username,
+//		@RequestParam("password")String password,
+//		Model model)
+//{
+//	User user=new User();
+//	user.setEmail(email);
+//	user.setPassword(password);
+//	user.setUsername(username);
+//	
+//	model.addAttribute("user", user);
+//	
+////	model.addAttribute("email",email);
+////	model.addAttribute("username",username);
+////	model.addAttribute("password",password);
+////	System.out.println(model);
+//	return "success";
+//}
